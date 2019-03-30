c376dc6277adae02b026416a5f0386075f10232a
rajithaeyee <rajithaeye@gmail.com>
2019-03-30 23:41:17 +0530

    added axios and db.json

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
 import { Layout } from 'antd';
+import axios from  'axios';
 
 const {
      Content,
   } = Layout;
 
 class Blog extends Component{
+    componentDidMount(){
+        axios.get('http://localhost:3001/posts').then(result=>{
+            console.log(result.data);
+        });
+    }
     render(){
         return(
             <h1>Blog</h1>
